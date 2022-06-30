932849b709dfb93b33cac99269119197bf4a340e
Jitendra Kumar <jitendr.kumar@zeemedia.esselgroup.com>
2022-06-30 22:04:45 +0530

    Remove Android Splash

--- android/app/src/main/java/com/tally/app/MainActivity.java
+++ android/app/src/main/java/com/tally/app/MainActivity.java
@@ -1,6 +1,25 @@
 package com.tally.app;
 
+import android.os.Build;
+import android.os.Bundle;
+import android.window.SplashScreenView;
+
+import androidx.core.view.WindowCompat;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        WindowCompat.setDecorFitsSystemWindows(getWindow(), false);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            getSplashScreen()
+                    .setOnExitAnimationListener(
+                            SplashScreenView::remove);
+        }
+
+        super.onCreate(savedInstanceState);
+    }
 }
